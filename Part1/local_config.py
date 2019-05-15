5a236ce9036cdbc37bc76df815f95e0db6b9b617
ChenmingXue <chenming.xue2011@gmail.com>
2019-05-14 20:11:13 -0700

    Add files via upload

---
+++ Part1/local_config.py
@@ -0,0 +1,5 @@
+cons_tok='YRGPYxGZePVLlMuTcKMMlSBgt'     #consumer token
+cons_sec='RdcY04wS3eDb9zdkZwLhuXFIyPIhlELtiydfH3OX7pTJLJeQVC'     #consumer secret
+
+app_tok='1127819119552630784-o29BzIYB41dYOXMvZAkYEa2ngwh9Yr'      #app token
+app_sec='dm1eu9Ea08jAqRNXIzNSwaqEOfzbSkwHZsGEgGJk1Tllo'      #app secret
